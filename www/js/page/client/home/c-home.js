54a91dce0f0494113033806f764d76da0890ca70
WebBestMaster <web.best.master@gmail.com>
2019-11-30 18:19:30 +0300

    refactor

--- www/js/page/client/home/c-home.js
+++ www/js/page/client/home/c-home.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React, {Component, type Node} from 'react';
+import {Link} from 'react-router-dom';
 
 // import type {RenderPageInputDataType} from '../../../component/app/render-route/render-route-type';
 import type {InitialDataType} from '../../../../../server/src/intial-data/intial-data-type';
@@ -8,6 +9,8 @@ import type {InitialDataType} from '../../../../../server/src/intial-data/intial
 import {routePathMap} from '../../../component/app/routes-path-map';
 import {getInitialClientData} from '../../../component/app/client-app-helper';
 import {isError} from '../../../lib/is';
+
+import type {MongoDocumentType} from '../../../../../server/src/database/database-type';
 
 import homeStyle from './home.scss';
 import imageLogo from './image/empty.jpg';
@@ -66,6 +69,28 @@ export class Home extends Component<PropsType, StateType> {
     };
 */
 
+    renderArticleLinkList(): Node {
+        const {state} = this;
+        const {initialContextData} = state;
+        const {rootPathData} = initialContextData;
+
+        if (!rootPathData) {
+            return null;
+        }
+
+        return (
+            <div>
+                {rootPathData.subDocumentList.map((article: MongoDocumentType): Node => {
+                    return (
+                        <Link key={article.slug} to={article.slug}>
+                            {article.title}
+                        </Link>
+                    );
+                })}
+            </div>
+        );
+    }
+
     render(): Node {
         const {state} = this;
         const {initialContextData} = state;
@@ -73,6 +98,9 @@ export class Home extends Component<PropsType, StateType> {
         return (
             <div>
                 <h1>Skazki</h1>
+                <hr/>
+                {this.renderArticleLinkList()}
+                <hr/>
                 <p>{JSON.stringify(initialContextData.rootPathData)}</p>
             </div>
         );
