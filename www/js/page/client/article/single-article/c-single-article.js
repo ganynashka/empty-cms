1947b2b05b25f01cafd8b4a454beb3be6570c9a4
WebBestMaster <web.best.master@gmail.com>
2019-12-26 19:59:23 +0300

    style article

--- www/js/page/client/article/single-article/c-single-article.js
+++ www/js/page/client/article/single-article/c-single-article.js
@@ -4,6 +4,9 @@ import React, {Component, type Node} from 'react';
 
 import {Markdown} from '../../../../component/layout/markdown/c-markdown';
 import type {InitialDataType} from '../../../../provider/intial-data/intial-data-type';
+import articleStyle from '../article.scss';
+
+import singleArticleStyle from './single-article.scss';
 
 type PropsType = {|
     +initialContextData: InitialDataType,
@@ -19,6 +22,21 @@ export class SingleArticle extends Component<PropsType, StateType> {
     }
 
     render(): Node {
-        return <h1>SingleArticle</h1>;
+        const {props} = this;
+        const {initialContextData} = props;
+        const {articlePathData} = initialContextData;
+
+        if (!articlePathData) {
+            return <h1 className={articleStyle.article__header}>Here is not list of link</h1>;
+        }
+
+        const {title, content} = articlePathData;
+
+        return (
+            <>
+                <h1 className={articleStyle.article__header}>{title}</h1>
+                <Markdown additionalClassName={singleArticleStyle.markdown} text={content}/>
+            </>
+        );
     }
 }
