49609ba57b4e379c0545facd5aa0702514865d44
WebBestMaster <web.best.master@gmail.com>
2020-01-03 19:45:48 +0300

    fix search

--- server/src/api/part/document-api.js
+++ server/src/api/part/document-api.js
@@ -233,7 +233,7 @@ export function addDocumentApi(app: $Application) {
 
         collection
             // $FlowFixMe
-            .find({$or: getSearchParameters(request)})
+            .find({$or: [...getSearchParameters(request)], isActive: true})
             .toArray((error: Error | null, documentList: Array<MongoDocumentType> | null) => {
                 if (error || !Array.isArray(documentList)) {
                     response.status(400);
