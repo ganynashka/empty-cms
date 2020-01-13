729d10425af54d36547e11aa33f0b9e9722d552a
WebBestMaster <web.best.master@gmail.com>
2020-01-13 20:59:44 +0300

    fix sh

--- mongo-stop.sh
+++ mongo-stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mongod mongod --shutdown --dbpath=db/db-1/data
-mongod mongod --shutdown --dbpath=db/db-2/data
-mongod mongod --shutdown --dbpath=db/db-3/data
-mongod mongod --shutdown --dbpath=db/db-4/data
+mongod --shutdown --dbpath=db/db-1/data
+mongod --shutdown --dbpath=db/db-2/data
+mongod --shutdown --dbpath=db/db-3/data
+mongod --shutdown --dbpath=db/db-4/data
