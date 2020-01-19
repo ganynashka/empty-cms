4fb17c4345eb6e787db60ae27d5f138fe5ee7bf7
WebBestMaster <web.best.master@gmail.com>
2020-01-19 18:19:50 +0300

    fix file loader

--- webpack/setting/module/rules.js
+++ webpack/setting/module/rules.js
@@ -51,7 +51,8 @@ module.exports.rules = [
             {
                 loader: 'base64-inline-loader',
                 query: {
-                    limit: isProduction ? 1e3 : 1, // 1k bytes for production
+                    limit: 1,
+                    // limit: isProduction ? 1 : 1, // 1k bytes for production
                     name: pathToLoadedFileFolder.replace(/^\//, '') + '/[name]-[md5:hash:hex:7].[ext]',
                 },
             },
