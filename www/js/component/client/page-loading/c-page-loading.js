0016331b47ee7d84fcb9b2bbc4c1ea26937e151a
WebBestMaster <web.best.master@gmail.com>
2020-01-21 20:11:24 +0300

    PageLoading - scroll to top

--- www/js/component/client/page-loading/c-page-loading.js
+++ www/js/component/client/page-loading/c-page-loading.js
@@ -3,16 +3,24 @@
 import React, {Component, type Node} from 'react';
 
 import loadingImage from '../header/image/favicon.svg';
+import {scrollToTop} from '../../../provider/screen/screen-context-helper';
 
 import pageLoadingStyle from './page-loading.scss';
 
+type StateType = null;
 type PropsType = {};
 
-export function PageLoading(props: PropsType): Node {
-    return (
-        <div className={pageLoadingStyle.page_loading__wrapper}>
-            <img alt="" className={pageLoadingStyle.page_loading__image} src={loadingImage}/>
-            <h4 className={pageLoadingStyle.page_loading__text}>Загрузка&hellip;</h4>
-        </div>
-    );
+export class PageLoading extends Component<PropsType, StateType> {
+    componentDidMount() {
+        scrollToTop();
+    }
+
+    render(): Node {
+        return (
+            <div className={pageLoadingStyle.page_loading__wrapper}>
+                <img alt="" className={pageLoadingStyle.page_loading__image} src={loadingImage}/>
+                <h4 className={pageLoadingStyle.page_loading__text}>Загрузка&hellip;</h4>
+            </div>
+        );
+    }
 }
