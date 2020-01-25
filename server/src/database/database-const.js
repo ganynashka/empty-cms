174acab3eb2590ebaaf6f33a3c7a0c5d6d48eaec
WebBestMaster <web.best.master@gmail.com>
2020-01-25 23:36:53 +0300

    refactor

--- server/src/database/database-const.js
+++ server/src/database/database-const.js
@@ -6,14 +6,13 @@ const databaseDumpFolderName = 'db-dump';
 export const dataBaseConst = {
     url: 'mongodb://localhost:27001,localhost:27002,localhost:27003,localhost:27004?replicaSet=MyBestReplica',
     shallCommand: {
-        backup:
-            `mkdir ${databaseDumpFolderName}; `
-            + [
-                'mongodump',
-                '--port=27001',
-                `--archive=${databaseDumpFolderName}/db-dump-\`date +%Y-%m-%d-%H-%M-%S\`.zip`,
-                `--db=${mainDataBaseName}`,
-            ].join(' '),
+        backup: [
+            `mkdir ${databaseDumpFolderName};`,
+            'mongodump',
+            '--port=27001',
+            `--archive=${databaseDumpFolderName}/db-dump-\`date +%Y-%m-%d--%H-%M-%S\`.zip`,
+            `--db=${mainDataBaseName}`,
+        ].join(' '),
     },
     name: mainDataBaseName,
     collection: {
