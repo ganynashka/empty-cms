ab414c61e705801c145ae9ae4d27b7d94e55a24a
WebBestMaster <web.best.master@gmail.com>
2020-02-08 00:05:47 +0300

    adsense - still begin

--- www/js/component/ads/adsense/ad-sense-helper.js
+++ www/js/component/ads/adsense/ad-sense-helper.js
@@ -6,9 +6,11 @@ export function getAdSenseAdsBlockHtml(adSlotId: number): string {
     return `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- bottom-ads -->
 <ins class="adsbygoogle"
-     style="display:inline-block;width:288px;height:258px"
-     data-ad-client="ca-pub-8997870404482178"
-     data-ad-slot="2979854461"></ins>
+     style="display:block"
+     data-ad-client="${googleAdSenseId}"
+     data-ad-slot="${adSlotId}"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>`;
