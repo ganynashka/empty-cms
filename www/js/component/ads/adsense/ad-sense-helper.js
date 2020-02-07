5dd9111faf5cf263ce580c445c25ac3d536484f4
WebBestMaster <web.best.master@gmail.com>
2020-02-08 00:13:58 +0300

    adsense - still begin

--- www/js/component/ads/adsense/ad-sense-helper.js
+++ www/js/component/ads/adsense/ad-sense-helper.js
@@ -3,6 +3,8 @@
 import {googleAdSenseId, isGoogleAdSenseInTestMode} from '../../../const';
 
 export function getAdSenseAdsBlockHtml(adSlotId: number): string {
+    return '';
+
     return `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- bottom-ads -->
 <ins class="adsbygoogle"
