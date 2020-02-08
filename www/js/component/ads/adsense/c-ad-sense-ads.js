1dda7a08ec5eff0876e2b5fddb2a713d87138c75
WebBestMaster <web.best.master@gmail.com>
2020-02-08 13:42:51 +0300

    adsense - still begin

--- www/js/component/ads/adsense/c-ad-sense-ads.js
+++ www/js/component/ads/adsense/c-ad-sense-ads.js
@@ -3,8 +3,10 @@
 /* global window */
 
 import React, {Component, type Node} from 'react';
+import AdSense from 'react-adsense';
 
 import {getAdSenseAdsBlockHtml} from './ad-sense-helper';
+import {googleAdSenseId} from '../../../const';
 
 type PropsType = {|
     +adSlotId: number,
@@ -19,6 +21,19 @@ export function AdSenseAds(props: PropsType): Node {
         return null;
     }
 
+    return (
+        <AdSense.Google
+            client={googleAdSenseId}
+            slot={adSlotId}
+            style={{display: 'block'}}
+            format='auto'
+            responsive='true'
+            layoutKey='-gw-1+2a-9x+5c'
+        />
+    );
+
+/*
     // eslint-disable-next-line react/no-danger, id-match
     return <div dangerouslySetInnerHTML={{__html: getAdSenseAdsBlockHtml(adSlotId)}} style={{width: 300}}/>;
+*/
 }
