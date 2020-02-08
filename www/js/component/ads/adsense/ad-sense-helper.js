a077f011e52bb0f66a4b8af942c803843ab11309
WebBestMaster <web.best.master@gmail.com>
2020-02-08 14:03:36 +0300

    adsense - still begin

--- www/js/component/ads/adsense/ad-sense-helper.js
+++ www/js/component/ads/adsense/ad-sense-helper.js
@@ -5,15 +5,18 @@ import {googleAdSenseId, isGoogleAdSenseInTestMode} from '../../../const';
 export function getAdSenseAdsBlockHtml(adSlotId: number): string {
     // return '';
 
-    return `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- bottom-ads -->
-<ins class="adsbygoogle"
-     style="display:block; height: 500px;"
-     data-ad-client="${googleAdSenseId}"
-     data-ad-slot="${adSlotId}"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>`;
+    return `
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <ins
+         class="adsbygoogle"
+         style="display:block"
+          data-ad-format="fluid"
+           data-ad-layout-key="-fn+4o+1n-cz+i5"
+             data-ad-client="${googleAdSenseId}"
+             data-ad-slot="${adSlotId}"
+            ></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+`;
 }
--- www/js/component/ads/adsense/c-ad-sense-ads.js
+++ www/js/component/ads/adsense/c-ad-sense-ads.js
@@ -3,10 +3,8 @@
 /* global window */
 
 import React, {Component, type Node} from 'react';
-import AdSense from 'react-adsense-ad';
 
 import {getAdSenseAdsBlockHtml} from './ad-sense-helper';
-import {googleAdSenseId} from '../../../const';
 
 type PropsType = {|
     +adSlotId: number,
@@ -21,24 +19,6 @@ export function AdSenseAds(props: PropsType): Node {
         return null;
     }
 
-    return (
-        <AdSense.Google
-            client={googleAdSenseId}
-            slot={adSlotId}
-            style={{ display: 'block' }}
-            format='auto'
-            responsive='true'
-/*
-            style={{display: 'block'}}
-            format='auto'
-            responsive='true'
-            layoutKey='-gw-1+2a-9x+5c'
-*/
-        />
-    );
-
-/*
     // eslint-disable-next-line react/no-danger, id-match
-    return <div dangerouslySetInnerHTML={{__html: getAdSenseAdsBlockHtml(adSlotId)}} style={{width: 300}}/>;
-*/
+    return <div dangerouslySetInnerHTML={{__html: getAdSenseAdsBlockHtml(adSlotId)}} style={{width: '100%'}}/>;
 }
